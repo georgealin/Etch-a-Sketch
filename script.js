17100131af2f3b6f8f1fc161da00904e9f4a1dae
georgealin <georgeepuran@gmail.com>
2023-07-12 14:09:16 +0300

    Add onmouseover function

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+//creates <div> and append it to .container
 function createDiv() {
     const newDiv = document.createElement('div');
     newDiv.classList.add('divs');
-    document.getElementsByClassName('container')[0].appendChild(newDiv);
+    document.querySelector('.container').appendChild(newDiv);
 };
 
-for (let i = 0; i < 256; i++) {
+let size = 32;
+for (let i = 0; i < size * size; i++) {
     createDiv();
 };
+
+let container =document.querySelector('.container');
+container.style.gridTemplateColumns = `repeat(${size}, 1fr)`; //add style to container div
+container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+let color = 'red';
+//it iterate on all divs and add onmouseover function to them.
+//the function change backgroundColor 
+const coloredDiv = document.getElementsByClassName('divs');
+for (let i = 0; i < coloredDiv.length; i++) {
+coloredDiv[i].onmouseover = function(e) {
+    if(e.buttons == 1) {
+    coloredDiv[i].style.backgroundColor = color;
+}};
+}
+
